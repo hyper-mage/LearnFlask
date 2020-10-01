f7c207584682024d84c39e8da6e6d87eeb4d3f41
hyper-mage <mlynn808138@gmail.com>
2020-10-01 14:13:55 -0500

    Did a little query stirng and request.args learning

--- app/app/views.py
+++ app/app/views.py
@@ -145,3 +145,15 @@ def create_entry():
     res = make_response(jsonify(req), 200)
 
     return res
+
+@app.route("/query")
+def query():
+
+    if request.args:
+        args = request.args
+        serialized = ', '.join(f"{k}: {v}" for k, v in args.items())
+        return f'(Query) {serialized}', 200
+
+    else:
+        return "No query received", 200
+
