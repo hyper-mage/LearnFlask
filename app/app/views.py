1cb43779e51829ecaf53102358ac9ca2b4054ddc
hyper-mage <mlynn808138@gmail.com>
2020-10-11 07:24:52 -0500

    Played around with cookies. How to set cookies in JS and python via flask, access them in the browser dev tool, delete them, and change their params.

--- app/app/views.py
+++ app/app/views.py
@@ -261,3 +261,37 @@ def get_report(path):
 
     except FileNotFoundError:
         abort(404)
+
+from flask import request, make_response
+
+@app.route('/cookies')
+def cookies():
+
+    res = make_response('cookies', 200)
+
+    cookies = request.cookies
+
+    flavor = cookies.get('flavor')
+    choc_type = cookies.get('chocolate type')
+    chewy = cookies.get('chewy')
+    brand = cookies.get('brand')
+
+    print(brand) 
+
+    res.set_cookie(
+        'flavor', 
+        value='chocolate chip',
+        max_age=10,
+        expires=None,
+        path=request.path,
+        domain=None,
+        secure=False,
+        httponly=False,
+        samesite=None
+        )
+
+    res.set_cookie('chocolate type', 'dark')
+    res.set_cookie('chewy', 'yes')
+
+    return res
+
